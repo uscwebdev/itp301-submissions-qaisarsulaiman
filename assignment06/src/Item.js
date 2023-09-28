80589692553b43243abef656f0b257ff087dc9ed
Qaisar Sulaiman <qsulaima@usc.edu>
2023-09-27 22:13:39 -0700

    Submit assignment06

---
+++ assignment06/src/Item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Item(prop) {
+  return (
+    <div className="col-sm-6 p-1">
+      <div className="col border rounded px-3 pt-3">
+        <div className="thumbnail">
+          <img
+            className="img-fluid rounded mb-2"
+            src={prop.img_src}
+            alt={prop.name}
+          />
+        </div>
+        <p className="float-end ms-4 text-secondary small">
+          <span className="fs-4 text-dark">{prop.price}</span> USD
+        </p>
+        <p className="mt-1">{prop.name}</p>
+      </div>
+    </div>
+  );
+}
